0fb27ae194c3dc8918fae5771c074a4b03d7868f
estherszliu <15076@coderacademy.edu.au>
2023-12-11 20:39:45 +1000

    created a write employee function

--- EmployeeDirectory.py
+++ EmployeeDirectory.py
@@ -1,6 +1,8 @@
+import csv
 filename = "storage.csv"
 
 class Employee:
+    # Constructor
     def __init__(self, employee_id, first_name, last_name, phone_number, job_title, salary, start_date):
         self.employee_id = employee_id
         self.first_name = first_name
@@ -9,7 +11,7 @@ class Employee:
         self.job_title = job_title
         self.salary = salary
         self.start_date = start_date
-
+    # print data from employee
     def __str__(self):
         return str(self.employee_id)  + " " + \
             self.first_name           + " " + \
@@ -19,13 +21,44 @@ class Employee:
             str(self.salary)          + " " + \
             self.start_date
 
-def write_employee(filename, employee):
-    pass
+    # get an employee as a list
+    def get_as_list(self):
+        return [
+            str(self.employee_id),
+            self.first_name,
+            self.last_name,
+            self.phone_number,
+            self.job_title,
+            str(self.salary),
+            self.start_date
+        ]
+    
+# write employee to file function
+def write_employees_csv(filename, employees):
+    
+    header = ["employee_id", "first_name", "last_name", "phone_number", "job_title", "salary","start_date"]
+    employee_data = []
+    
+    for employee in employees:
+        employee_data.append(employee.get_as_list())
 
-def read_employees(filename):
+    with open (filename, "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(employee_data)
+
+def read_employees_csv(filename):
+    # search employee in employees
+
+    #if the found the employee
+
+    # print employee
     pass
 
 employee1 = Employee(1, "Esther", "Dennis", "035424445", "barista", "65000", "01/08/2022" )
-print(employee1)
 
-write_employee(filename, employee1)
+
+
+write_employees_csv(filename, [employee1])
+
+
